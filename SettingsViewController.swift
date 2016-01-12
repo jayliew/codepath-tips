0bc6100dda293a8596a96f0f600b81679abe9f8e
Jay Liew <jayliew@jayliew.com>
2016-01-12 12:07:07 -0800

    settings button launches blank settings view controller with no problems, no error messages

---
+++ SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  tips
+//
+//  Created by Jay Liew on 1/12/16.
+//  Copyright © 2016 Jay Liew. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController{
+
+}
