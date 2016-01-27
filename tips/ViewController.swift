62c2a5889da548ec47f39e8b7b16969e841a90d1
Jay Liew <jayliew@jayliew.com>
2016-01-27 14:23:03 -0800

    billField animation floats up upon input. When cleared of a value, it floats down back to original position

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipTextLabel: UILabel!
     @IBOutlet weak var totalTextLabel: UILabel!
     
-    @IBOutlet weak var separatorView: UIView!
+
     @IBOutlet weak var billAmountLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,30 +31,24 @@ class ViewController: UIViewController {
         super.viewWillAppear(animated)
         let defaults = NSUserDefaults.standardUserDefaults()
         tipControl.selectedSegmentIndex = defaults.integerForKey("default_tip_index")
-        onEditingChanged(billField)
+//        onEditingChanged(billField)
         
         if defaults.boolForKey("dark_theme") == true {
             self.view.backgroundColor = UIColor(red: 0.0235, green: 0, blue: 0.3765, alpha: 1.0)
             billField.backgroundColor = UIColor(red: 0.0235, green: 0, blue: 0.3765, alpha: 1.0)
-            
             tipTextLabel.textColor = UIColor(red: 0, green: 0.5647, blue: 1, alpha: 1.0)
             totalTextLabel.textColor = UIColor(red: 0, green: 0.5647, blue: 1, alpha: 1.0)
-            separatorView.backgroundColor = UIColor(red: 0, green: 0.5647, blue: 1, alpha: 1.0)
             billField.textColor = UIColor.whiteColor()
             billField.backgroundColor = UIColor(red: 0, green: 0.5647, blue: 1, alpha: 1.0)
-            billAmountLabel.textColor = UIColor(red: 0, green: 0.5647, blue: 1, alpha: 1.0)
             tipLabel.textColor = UIColor(red: 0, green: 0.5647, blue: 1, alpha: 1.0)
             totalLabel.textColor = UIColor(red: 0, green: 0.5647, blue: 1, alpha: 1.0)
         }else{
             self.view.backgroundColor = UIColor.whiteColor()
             billField.backgroundColor = UIColor.whiteColor()
-            
             tipTextLabel.textColor = UIColor.blackColor()
             totalTextLabel.textColor = UIColor.blackColor()
-            separatorView.backgroundColor = UIColor.blackColor()
             billField.textColor = UIColor.blackColor()
             billField.backgroundColor = UIColor.whiteColor()
-            billAmountLabel.textColor = UIColor.blackColor()
             tipLabel.textColor = UIColor.blackColor()
             totalLabel.textColor = UIColor.blackColor()
             
@@ -67,15 +61,26 @@ class ViewController: UIViewController {
     }
 
     @IBAction func onEditingChanged(sender: AnyObject) {
+
+        if billField.center.y == 200.0 && billField.text != "" {
+            UIView.animateWithDuration(0.5, delay: 0.0,
+                options: .CurveEaseInOut, animations:{
+                    self.billField.center.y -= 87
+            }, completion: nil)
+        }else if billField.center.y != 200.0 && billField.text == ""{
+            UIView.animateWithDuration(0.5, delay: 0.0,
+                options: .CurveEaseInOut, animations:{
+                    self.billField.center.y += 87
+                }, completion: nil)
+        }
+        
+        
         let billAmount = NSString(string: billField.text!).doubleValue
         let tipPercentages = [0.18, 0.2, 0.22]
         let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
         
         let tip = billAmount * tipPercentage
         let total = billAmount + tip
-        
-//        tipLabel.text = "$\(tip)"
-  //      totalLabel.text = "$\(total)"
         
         tipLabel.text = String(format: "$%.2f",tip)
         totalLabel.text = String(format: "$%.2f", total)
